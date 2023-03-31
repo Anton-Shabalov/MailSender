108b868ef3d1ce03dbc791069a07e789e17d5cf9
Крисанов Роман Валерьевич <284712@niuitmo.ru>
2023-04-01 02:33:28 +0300

    Working scheduler of email sending

--- src/main/java/com/mailSender/MailSender/Controller/MailController.java
+++ src/main/java/com/mailSender/MailSender/Controller/MailController.java
@@ -2,8 +2,9 @@ package com.mailSender.MailSender.Controller;
 
 import com.mailSender.MailSender.DTO.Message;
 import com.mailSender.MailSender.DTO.Response;
-import com.mailSender.MailSender.service.interfaces.SendEmail;
+import com.mailSender.MailSender.scheduling.EmailJobFactory;
 import jakarta.validation.Valid;
+import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,11 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/mail")
 public class MailController {
+
+
     @Autowired
-    SendEmail emailpush;
+    private EmailJobFactory emailJobFactory;
+
     @PostMapping("/sendMail")
-    public ResponseEntity<Response> sendMail(@Valid @RequestBody Message message) {
-        emailpush.sendMail(message);
+    public ResponseEntity<Response> sendMail(@Valid @RequestBody Message message) throws SchedulerException {
+        emailJobFactory.ScheduleEmailSending(message);
         return ResponseEntity.ok(new Response(""));
     }
 
