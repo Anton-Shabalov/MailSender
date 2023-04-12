f5432cc224b7992d932f640ebf0004f8bc3661b8
Anton-Shabalov <anton.shabalov@icloud.com>
2023-04-12 13:31:41 +0300

    На растерзание Роме

--- src/main/java/com/mailSender/MailSender/scheduling/EmailJobFactory.java
+++ src/main/java/com/mailSender/MailSender/scheduling/EmailJobFactory.java
@@ -14,15 +14,17 @@ import java.time.temporal.ChronoUnit;
 import java.util.Date;
 @Component
 public class EmailJobFactory {
+    private int countMessage=0;
     @Autowired
     private ApplicationContext applicationContext;
     private JobDetail createEmailSendJobDetail(Message message) {
+
         JobDetailFactoryBean factory = new JobDetailFactoryBean();
         factory.setJobClass(EmailSendJob.class);
         factory.setDescription("Email Sender");
         factory.setDurability(true);
         factory.setApplicationContext(applicationContext);
-        factory.setName("Email Sender Job");
+        factory.setName("Email Sender"+Instant.now().toString());
         JobDataMap jobDataMap = new JobDataMap();
         jobDataMap.put("emailTo",message.getEmailTo());
         jobDataMap.put("subject",message.getSubject());
@@ -38,10 +40,10 @@ public class EmailJobFactory {
         SimpleTriggerFactoryBean factory = new SimpleTriggerFactoryBean();
         factory.setJobDetail(emailSendJobDetail);
         // TODO change to interval, corresponding to actual business-process requirements
-        factory.setStartTime(Date.from(Instant.now().plus(100, ChronoUnit. SECONDS)));
+        factory.setStartTime(Date.from(Instant.now().plus(10, ChronoUnit. SECONDS)));
         factory.setRepeatCount(0);
         factory.setRepeatInterval(100);
-        factory.setName("Email Sender Trigger");
+        factory.setName("Email Sender Trigger"+ Instant.now());
         factory.afterPropertiesSet();
         return factory.getObject();
     }
@@ -50,6 +52,7 @@ public class EmailJobFactory {
     Scheduler scheduler;
 
     public void ScheduleEmailSending(Message message) throws SchedulerException {
+
         JobDetail emailSendJobDetail = createEmailSendJobDetail(message);
         Trigger emailSendTrigger = createEmailSendJobTrigger(emailSendJobDetail);
         scheduler.scheduleJob(emailSendJobDetail,emailSendTrigger);
